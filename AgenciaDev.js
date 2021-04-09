0cee0638bafa041c4d602d2794f41674aa8efbb4
YakuzaGX <81425202+YakuzaGX@users.noreply.github.com>
2021-04-08 23:31:26 -0300

    Add files via upload

---
+++ AgenciaDev.js
@@ -0,0 +1,7 @@
+*/
+1- mostre 3 destinos para o cliente
+2- deixe o cliente escolher um dos lugares
+3- apresente o (ou os) pacotes para a escolha do cliente
+4- depois que ele escolher // boa viagem (se quiser incremente mais como dito acima)
+*/
+
