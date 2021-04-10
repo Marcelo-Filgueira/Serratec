0e58c05ddaf29d403050f492f6cb4764e5cd4f49
YakuzaGX <81425202+YakuzaGX@users.noreply.github.com>
2021-04-10 19:43:36 -0300

    Add files via upload

---
+++ Fase 1 e 2/Carta.js
@@ -0,0 +1,15 @@
+/*
+Escreva uma carta para você mesmo ler ao final do curso, e imprima no console. Na carta você deve incluir as suas expectativas para o curso de programação e sua carreira nesta nova área. Seja criativo, lembre-se de incluir desafios e incentivos, para que o seu eu do futuro tenha orgulho de suas conquistas!
+*/
+
+
+
+    console.log("Carta para a “EU” do final do SERRATEC\n");
+    console.log("Olá Marcelo, como você passou esses meses? tenho certeza que bem desesperado né?");
+    console.log("Porém fique tranquilo, sei que apesar de todos os problemas e dificuldade,.");
+    console.log("Algo muito bom e promissor te aguarda neste novo futuro que você está buscando.");
+    console.log("Não se esqueça das pessoas que te apoiaram nesse tempo todo.");
+    console.log("colque em prática tudo que você conseguiu nesses meses e siga esse futuro")
+    console.log(" Att,");
+    console.log("  Marcelo Renato Filgueira");
+    console.log("Teresópolis; 10 de abril de 2021");
