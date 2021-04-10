ee260fda43d8a40432826a2a8935ff80371f2eda
YakuzaGX <81425202+YakuzaGX@users.noreply.github.com>
2021-04-10 19:39:16 -0300

    Delete Fase 6 directory

--- Fase 6/ContadorDePessoas.js
+++
@@ -1,5 +0,0 @@
-/*
-Funcionário vai entregar uma plaquinha de cartolina amarela para cada cliente que entrar no mercado para controlar o número de pessoas na loja.
-*/
-
-var total = 0;
--- Fase 6/LoopEncadeado.js
+++
@@ -1,7 +0,0 @@
-
-
-for (var i = 1; i <= 5; i ++){
-    console.log("novo i", i);
-}
-  for (var j = 5; j > 0; j--)
-    console.log ("i", i, "j", j);
